d9c812d283f94a96a6cd0aefb244534af1b91d6f
Ryo Kato <katoryo55@gmail.com>
2015-08-02 15:16:45 +0900

    不要なコードを削除

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 var casper = require('casper').create({
   verbose: true,
-  logLevel: "debug"
+  logLevel: 'debug'
 });
 
 var fs = require('fs');
-casper.start("http://www.pexels.com/search/fire/");
+casper.start('http://www.pexels.com/search/fire/');
 
 var maxNumOfImages = 0;
 var srcs = [];
 
-for (i = 0; i < 10; i++) {
-  var images = [];
+for (i = 0; i < 20; i++) {
   casper.then(function() {
     this.wait(1000);
-    var that = this;
     srcs = this.evaluate(function() {
-      window.scrollBy(0, 1000);
+      window.scrollBy(0, 2000);
       var images = document.getElementsByTagName('img');
       var src_list = [];
       for (var i = 0; i < images.length; i++) {
@@ -23,33 +21,11 @@ for (i = 0; i < 10; i++) {
       }
       return src_list;
     });
-    //casper.log(images, 'debug');
-    // if (images !== null) {
-    //   for (var i = 0; i < images.length; i++) {
-    //     casper.log(images[i], 'debug');
-    //   }
-    // }
   });
-  // casper.then(function() {
-  //   if (maxNumOfImages >= images.length) {
-  //     var image_array = Array.prototype.slice.call(images);
-  //     var srcs = [];
-  //     image_array.forEach(function(image) {
-  //       if (image === null) {
-  //         return;
-  //       }
-  //       srcs.push(image.src);
-  //     });
-  //     didFetchedAllImages = true;
-  //     var f = fs.open('result.txt', 'w');
-  //     f.write(srcs.join('\n'));
-  //     f.close();
-  //   };
-  //   maxNumOfImages = images.length;
-  // });
-  // if (didFetchedAllImages === true) {
-  //   break;
-  // }
+  if(srcs.length === maxNumOfImages) {
+    break;
+  }
+  maxNumOfImages = srcs.length;
   console.log('looping...');
 };
 
