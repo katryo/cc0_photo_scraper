0f54ba1f6b39fa492ab1051d84336966183c48a5
Ryo Kato <katoryo55@gmail.com>
2015-08-02 16:34:02 +0900

    phantomJSを別インスタンスで作らないといけないのでダメ

--- index.js
+++ index.js
@@ -13,37 +13,37 @@ var fs = require('fs');
 var targetsJson = require('./targets.json');
 var targets = targetsJson.targets;
 targets.forEach(function(query, index, array) {
-  casper.open('http://www.pexels.com/search/' + query + '/');
-
-  var maxNumOfImages = 0;
   var srcs = [];
+  casper.thenOpen('http://www.pexels.com/search/' + query + '/', function() {
+    var maxNumOfImages = 0;
 
-  for (var i = 0; i < 20; i++) {
-    casper.then(function() {
-      this.wait(1000);
-      srcs = this.evaluate(function() {
-        window.scrollBy(0, 1000);
-        var images = document.getElementsByTagName('img');
-        var src_list = [];
-        for (var i = 0; i < images.length; i++) {
-          src_list.push(images[i].src);
-        }
-        return src_list;
+    for (var i = 0; i < 20; i++) {
+      casper.then(function() {
+        this.wait(1000);
+        srcs = this.evaluate(function() {
+          window.scrollBy(0, 1000);
+          var images = document.getElementsByTagName('img');
+          var src_list = [];
+          for (var i = 0; i < images.length; i++) {
+            src_list.push(images[i].src);
+          }
+          return src_list;
+        });
       });
-    });
-    if (srcs.length === maxNumOfImages) {
-      break;
-    }
-    maxNumOfImages = srcs.length;
-    console.log('looping...');
-  };
+      if (srcs.length === maxNumOfImages) {
+        break;
+      }
+      maxNumOfImages = srcs.length;
+      console.log('looping...');
+    };
+  });
 
   casper.then(function() {
-    var f = fs.open(query + '.txt', 'w');
+    var f = fs.open('results/' + query + '.txt', 'w');
     f.write(srcs.join('\n'));
     f.close();
   });
 
+  casper.run();
 
 });
-casper.run();
