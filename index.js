da85144df1b0249f426c0e77ac3bd44c73e938fe
Ryo Kato <katoryo55@gmail.com>
2015-08-02 15:10:59 +0900

    スクレイピング成功

--- index.js
+++ index.js
@@ -4,33 +4,59 @@ var casper = require('casper').create({
 });
 
 var fs = require('fs');
-casper.start("http://www.pexels.com/search/horror/");
+casper.start("http://www.pexels.com/search/fire/");
 
 var maxNumOfImages = 0;
+var srcs = [];
 
-var didFetchedAllImages = false;
-while (didFetchedAllImages === false) {
+for (i = 0; i < 10; i++) {
+  var images = [];
   casper.then(function() {
     this.wait(1000);
-    images = this.evaluate(function(y) {
-      window.scrollBy(0, y);
-      var images = document.querySelectorAll('img');
-      return images;
-    }, 4000);
-    if (maxNumOfImages >= images.length) {
-      var srcs = images.map(function(image) {
-        return image.src;
-      });
-      didFetchedAllImages = true;
-    };
-    casper.log(didFetchedAllImages, 'debug');
-    maxNumOfImages = images.length;
-    casper.log(maxNumOfImages, 'debug');
+    var that = this;
+    srcs = this.evaluate(function() {
+      window.scrollBy(0, 1000);
+      var images = document.getElementsByTagName('img');
+      var src_list = [];
+      for (var i = 0; i < images.length; i++) {
+        src_list.push(images[i].src);
+      }
+      return src_list;
+    });
+    //casper.log(images, 'debug');
+    // if (images !== null) {
+    //   for (var i = 0; i < images.length; i++) {
+    //     casper.log(images[i], 'debug');
+    //   }
+    // }
   });
-  if(didFetchedAllImages === true) {
-    break;
-  }
+  // casper.then(function() {
+  //   if (maxNumOfImages >= images.length) {
+  //     var image_array = Array.prototype.slice.call(images);
+  //     var srcs = [];
+  //     image_array.forEach(function(image) {
+  //       if (image === null) {
+  //         return;
+  //       }
+  //       srcs.push(image.src);
+  //     });
+  //     didFetchedAllImages = true;
+  //     var f = fs.open('result.txt', 'w');
+  //     f.write(srcs.join('\n'));
+  //     f.close();
+  //   };
+  //   maxNumOfImages = images.length;
+  // });
+  // if (didFetchedAllImages === true) {
+  //   break;
+  // }
   console.log('looping...');
 };
+
+casper.then(function() {
+  var f = fs.open('result.txt', 'w');
+  f.write(srcs.join('\n'));
+  f.close();
+});
 
 casper.run();
